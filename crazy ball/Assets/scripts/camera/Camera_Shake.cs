3ca644027548325bd60ba16e9eb13a15dffff2ac
Arunabh-gupta <87766488+Arunabh-gupta@users.noreply.github.com>
2022-03-18 16:18:15 +0530

    Update Camera_Shake.cs

--- crazy ball/Assets/scripts/camera/Camera_Shake.cs
+++ crazy ball/Assets/scripts/camera/Camera_Shake.cs
@@ -5,7 +5,9 @@ public class Camera_Shake : MonoBehaviour
 {
     //The idea is to attach a spring joint to the camera gameobject and give some force to it so that it can come back and forth to create the effect
 
-    // here the Main camera is made the child of an empty game object(camera_follower) and camera follow script is attached on that. The camera_follower game object has given a rigidbody2d and that rigidbody is given to the attached rigidbody field in the spring joint in the MainCamera. Now the magnitude field is given a value of 50 and the linear drag of the camera rigidbody is increased to 15 so that the shaking can stop after a moment.   
+    // Here the Main camera is made the child of an empty game object(camera_follower) and camera follow script is attached on that. 
+    // The camera_follower game object has given a rigidbody2d and that rigidbody is given to the attached rigidbody field in the spring joint in the MainCamera. 
+    // Now the magnitude field is given a value of 50 and the linear drag of the camera rigidbody is increased to 15 so that the shaking can stop after a moment.   
     SpringJoint2D shakeJoint;
     private void Start()
     {
